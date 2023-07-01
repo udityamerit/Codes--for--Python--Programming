ffc43f908edca0afa7ed9286baf6e1d2d35c2659
Uditya Narayan Tiwari <uditmerit@gmail.com>
2023-07-01 10:21:40 +0530

    add python

---
+++ Practice_set_5_dictionary_set.py
@@ -0,0 +1,79 @@
+# Question 2
+# s = set()
+# print("Enter the numbers:")
+# a = input()
+# s.add(a)
+# b = input()
+# s.add(b)
+# c = input()
+# s.add(c)
+# d = input()
+# s.add(d)
+# e = input()
+# s.add(e)
+# f = input()
+# s.add(f)
+# print(s)
+
+# Question 3 
+# s = set()
+# s.add(18)
+# s.add("18") # set can contain int and string 
+# print(s)
+# print(type(s))
+
+
+
+# Question 4
+# s= set()
+# s.add(20)
+# s.add(20.0)
+# s.add("20")
+# print(s)
+# print(len(s))
+
+# Question 5
+# s={}
+# print(type(s)) # here s is our dict
+
+# Question 6
+# dict = { }
+# print("Enter the keys and value:")
+# # f1 = input()
+# dict.update({"ram":"hindi"})
+# # f2 = input()
+# dict.update({"sam":"urdu"})
+# # f3 = input()
+# dict.update({"raju":"hurdu"})
+# # f4 = input()
+# dict.update({"ramesh":"bangali"})
+# print(dict)
+
+# Question 7
+# dict = { }
+# print("Enter the keys and value:")
+# # f1 = input()
+# dict.update({"sam":"hindi"})
+# # f2 = input()
+# dict.update({"sam":"urdu"})
+# # f3 = input()
+# dict.update({"raju":"hurdu"})
+# # f4 = input()
+# dict.update({"ramesh":"bangali"})
+# print(dict)
+# concusion:if you change the unique name of the two friends and give the same name both of them you will see the output is considered only one name in the final dictionary 
+
+# Question 8
+# dict = { }
+# print("Enter the keys and value:")
+# # f1 = input()
+# dict.update({"sam":"hindi"})
+# # f2 = input()
+# dict.update({"ram":"hindi"})
+# # f3 = input()
+# dict.update({"raju":"hurdu"})
+# # f4 = input()
+# dict.update({"ramesh":"bangali"})
+# print(dict)
+# conclusion: if two language of the friends is same there is no difference in the dictionary , output is the same as that dictionary you have to created
+ 
