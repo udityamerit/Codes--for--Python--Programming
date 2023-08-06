88b00e1c93d5b8e9d2ee4fc2324ca5f3578cb22c
Uditya Narayan Tiwari <uditmerit@gmail.com>
2023-08-06 13:54:54 +0530

    add game 3.1 in python

--- Python Project/3.1.Snake Water Gun Game_Version2.0.py
+++ Python Project/3.1.Snake Water Gun Game_Version2.0.py
@@ -1,10 +1,12 @@
+
 # Snake Water Gun Game:
+
 import random
 
-num = random.randint(0, 2)
+
 
 print("Welcome to Snack-Water and Gun game: ")
-print("Number_of_chances is only 10")
+chance = int(input("Number_of_chances do you want to play: "))
 comp = []
 User = []
 draw = []
@@ -13,10 +15,12 @@ def game(num, user):
     a = "Match is draw"
     b = "computer won the game"
     c = "You won the game"
+
     if user >2:
-        print("You entered the invalid input:\n Please enter the valid input:")     
+        print("You entered the invalid input:\n Please enter the valid input:") 
+
     elif num == user:
-        print(a)
+        print(a) 
         draw.append(a)
     elif num == 0 and user == 1:
         print(b)
@@ -30,17 +34,19 @@ def game(num, user):
     else:
         print(c)
         User.append(c)
+       
 
 
 Number_of_chances = 1
-while Number_of_chances <= 10:
+while Number_of_chances <= chance:
+    num = random.randint(0, 2)
     print(" Plese Enter the: \n 0 for snake: \n 1 for Water:\n 2 for Gun:")
     user = int(input("Select the number:"))
     print("comp is selected:", num)
     print("user is selected:", user)
 
     game(num, user)
-    print("Number_of_chances left", Number_of_chances - 10)
+    print("Number_of_chances left", Number_of_chances - chance)
     Number_of_chances = Number_of_chances + 1
     print(" ")
 
@@ -57,6 +63,6 @@ if len(comp)>len(User):
 elif len(comp)<len(User):
     print(" Finaly You won the game")
 else:
-    print("Game is draw")
+    print(" Fanaly Game is draw")
 print(" ")
 print("Game is ended")
