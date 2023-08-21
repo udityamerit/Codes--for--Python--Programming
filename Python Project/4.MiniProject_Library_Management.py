88b1ba057cbcf0e16cf1d6c12dc60b5de088df50
Uditya Narayan Tiwari <uditmerit@gmail.com>
2023-08-21 22:09:54 +0530

    modify the mini1

--- Python Project/4.MiniProject_Library_Management.py
+++ Python Project/4.MiniProject_Library_Management.py
@@ -1,14 +1,15 @@
-'''Project to make a library Management system
-Using OOPs '''
+'''Project to make a library Management system'''
 
 class Library:
 #     list1 = ["C","C++","Python","Java","Math","Physics"]
     def __init__(self):
+        self.name = input("Enter your name: ")
         self.list = ["C","C++","Python","Java","Math","Physics"]
         self.list2 = self.list
 
     def display_book(self):
-        return f"list of books which present in the library: {self.list2}"
+        return f"list of books which is present in your library: {self.list2}"
+    
 
 
     def add_book(self):
@@ -20,7 +21,7 @@ class Library:
          i = i+1
 
         print("Book is added...")
-        return f"List of books in the library: {self.list2}"
+        return f"List of books in your library: {self.list2}"
     
     def lend_book(self):
         n = int(input("Enter the number of books do you want to buy:"))
@@ -34,20 +35,45 @@ class Library:
              i = 0
              while(i<n):
                 print( f"You bought the {self.buy} book")
+                self.list2.remove(element)
                 i= i+1
+                print(f"List of books in your library: {self.list2}")
              break
         else:
             print("This Book is not present in the library: ")
-            print(f"Present books in the Library is : {self.list2}")
-
-        
+            print(f"Present books in your Library is : {self.list2}")
     
+    def restore_book(self):
+       self.restore = input("Return the buy book to the Library: ")
+       self.list2.append(self.restore)
+       print(f"Reurning the {self.buy} book: \n Your Library book list is: {self.list2}") 
+    
+
+print(" ")
+print("**********Welcome to the Library managment system:**********")
+print(" ")
 li = Library()
-print(li.display_book())
-print(" ")
-
-print(li.add_book())
-print(" ")
-
-print(li.lend_book())
-print(" ")
+i = 0
+while(True):
+   print(f"Enter the Number to Proceed ==> \n 1 for add the book in Library:\n 2 for buy the book from the Library:\n 3 for return the book to the Library:\n 4 for Display the books present in the Library:\n 0 for quit the Library")
+   print(" ")
+   n = int(input("Enter the number: "))
+   if n == 1:
+      print(li.add_book())
+      print(" ")
+    
+   elif n == 2:
+      print(li.lend_book())
+      print(" ")
+  
+   elif n == 3:
+      print(li.restore_book())
+      print(" ")
+    
+   elif n == 4:
+      print(li.display_book())
+      print(" ")
+  
+   else:
+      print("Quiting the Library:==> *****Have a nice day****")
+      break
