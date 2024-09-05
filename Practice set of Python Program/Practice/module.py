ed29a5bcacb8cceed1c5369fce2aa7bbfb615867
Uditya Narayan Tiwari <uditmerit@gmail.com>
2024-09-05 20:23:15 +0530

    adding the content

--- Practice set of Python Program/Practice/module.py
+++ Practice set of Python Program/Practice/module.py
@@ -71,63 +71,63 @@
 
 
 
-# from tkinter import *
+from tkinter import *
 
-# root = Tk()
-# root.title("GUI dashboard")
-# root.geometry("500x500")
-# root.config(background="lightblue")
+root = Tk()
+root.title("GUI dashboard")
+root.geometry("500x500")
+root.config(background="lightblue")
 
-# def detail():
-#         print(f"name: {namevalue}\n mobile no: {mobvalue}\n Price: {pricevalue}")
+def detail():
+        print(f"name: {namevalue}\n mobile no: {mobvalue}\n Price: {pricevalue}")
 
-#         with open("gui.txt","a") as f:
-#                 f.write(f"name: {namevalue}\n mobile no: {mobvalue}\n Price: {pricevalue}")
+        with open("gui.txt","a") as f:
+                f.write(f"name: {namevalue}\n mobile no: {mobvalue}\n Price: {pricevalue}")
 
-# def exit():
-#         root.destroy()
+def exit():
+        root.destroy()
 
-# Label(root,text="My first menucard",font="robotomono,25,bold",borderwidth=5,foreground="black",bg="gold").grid(row=0,column=2)
+Label(root,text="My first menucard",font="robotomono,25,bold",borderwidth=5,foreground="black",bg="gold").grid(row=0,column=2)
 
-# # name of some menu 
-# Label(root,text="Name ",font=("robotomono",15,"bold"),bg="white",fg="black",borderwidth=5).grid(row=1,column=2,pady=10)
+# name of some menu 
+Label(root,text="Name ",font=("robotomono",15,"bold"),bg="white",fg="black",borderwidth=5).grid(row=1,column=2,pady=10)
 
-# Label(root,text="mobile",font="robotomono,15,bold",bg="white",fg="black",borderwidth=5).grid(row=2,column=2,pady=10)
+Label(root,text="mobile",font="robotomono,15,bold",bg="white",fg="black",borderwidth=5).grid(row=2,column=2,pady=10)
 
-# Label(root,text="Price",font="robotomono,15,bold",fg="black",bg="white",borderwidth=5).grid(row=3,column=2,pady=10)
+Label(root,text="Price",font="robotomono,15,bold",fg="black",bg="white",borderwidth=5).grid(row=3,column=2,pady=10)
 
-# # user input
-# namevalue = StringVar()
-# mobvalue = StringVar()
-# pricevalue = IntVar()
+# user input
+namevalue = StringVar()
+mobvalue = StringVar()
+pricevalue = IntVar()
 
-# #Entry
+#Entry
 
-# Entry(root,textvariable=namevalue,font="robotomono,15,bold",fg="blue").grid(row=1,column=3)
-# Entry(root,textvariable=mobvalue,font="robotomono,15,bold",fg="blue").grid(row=2,column=3)
-# Entry(root,textvariable=pricevalue,font="robotomono,15,bold",fg="blue").grid(row=3,column=3)
-
-
-# Button(root,text="Submit",font="robotomono,15",background="green",fg="white",relief="raised",borderwidth=5,command=detail).grid(column=3)
-
-# Button(root,text="Exit",font="robotomono,15,bold",fg="red",bg="white",relief="groove",borderwidth=5,command=exit).grid(column=3,pady=15)
+Entry(root,textvariable=namevalue,font="robotomono,15,bold",fg="blue").grid(row=1,column=3)
+Entry(root,textvariable=mobvalue,font="robotomono,15,bold",fg="blue").grid(row=2,column=3)
+Entry(root,textvariable=pricevalue,font="robotomono,15,bold",fg="blue").grid(row=3,column=3)
 
 
-# root.mainloop()
+Button(root,text="Submit",font="robotomono,15",background="green",fg="white",relief="raised",borderwidth=5,command=detail).grid(column=3)
+
+Button(root,text="Exit",font="robotomono,15,bold",fg="red",bg="white",relief="groove",borderwidth=5,command=exit).grid(column=3,pady=15)
 
 
+root.mainloop()
 
 
 
 
-mat1 = [[0,0,0],
-        [0,0,0],
-        [0,0,0]]
 
-for i in range(len(mat1)):
-        for j in range(len(mat1[i])):
-                c = int(input(f"Plese enter the value at the index of matrix: {i,j} : "))
-                mat1[i][j]=c
+
+# mat1 = [[0,0,0],
+#         [0,0,0],
+#         [0,0,0]]
+
+# for i in range(len(mat1)):
+#         for j in range(len(mat1[i])):
+#                 c = int(input(f"Plese enter the value at the index of matrix: {i,j} : "))
+#                 mat1[i][j]=c
                 
 
-print(mat1)
+# print(mat1)
