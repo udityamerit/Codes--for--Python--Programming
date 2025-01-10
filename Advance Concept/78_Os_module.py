7e8c6b18d0909041835ff615a4c89a5cbbd4a018
Uditya Narayan Tiwari <uditmerit@gmail.com>
2025-01-10 12:44:25 +0530

    adding the os module

---
+++ Advance Concept/78_Os_module.py
@@ -0,0 +1,17 @@
+import os
+
+# print(dir(os))
+
+# print(os.getcwd())   # it is give the current directory name where the programm is running
+# print(os.listdir())   # it is give the list of current folder where the program is stored
+
+# print(os.listdir("C://")) # it is print the folders name which is present in the C drive
+
+# os.mkdir("hello")  # this is create a folder/directory 
+
+# os.makedirs("this/that/how") # this is make the subdirecotries like this-->that-->how followed by the "/" as given command 
+
+# os.rename("hello.txt", "Hello.txt")
+
+# print(os.path.exists("C://")) # this is for the checking if the file is exists or not in our system 
+
