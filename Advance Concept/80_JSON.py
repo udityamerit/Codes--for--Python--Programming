59507ca00ef0cdd0c9b732aa0abc29abf2f16414
Uditya Narayan Tiwari <uditmerit@gmail.com>
2025-01-10 20:44:02 +0530

    json module

--- Advance Concept/80_JSON.py
+++ Advance Concept/80_JSON.py
@@ -3,3 +3,18 @@ import json
 data = '{"var1": 54, "var2": "harry"}'
 parsed = json.loads(data)
 print(parsed)
+
+
+data2 = {
+        "channal_name": "code_fusion",
+        "cars": ["bmw", "audi a8", "farrari"],
+        "fridge":("roti", 540),
+        "isbad": False
+}
+
+jasoncomb = json.dumps(data2) # it is convert the data to the javascript compatible from the python
+
+print(jasoncomb)
+
+jsa = json.dumps(data2, sort_keys=True)
+print(jsa)
